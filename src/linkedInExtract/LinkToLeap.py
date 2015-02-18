29b982ff3b86bab8d2d82ef777e8941de613d8c3
Kasper Passov <pvx884@alumni.ku.dk>
2015-02-18 16:13:15 +0100

    Jeg pusher for Lasse

--- src/linkedInExtract/LinkToLeap.py
+++ src/linkedInExtract/LinkToLeap.py
@@ -1,9 +1,40 @@
-from xml.dom import minidom
+# from xml.dom import minidom
+# from BeautifulSoup import BeautifulSoup
+import xml.etree.ElementTree as ET
 
-xmldoc = minidom.parse('test.xml')
-print xmldoc
-print getText(xmldoc.childNodes)
-print xmldoc.childNodes.childNodes
+xml = """ <?xml version="1.0" encoding="UTF-8"?>
+            <person>
+                <id>1R2RtA</id>
+                <first-name>Frodo</first-name>
+                <last-name>Baggins</last-name>
+                <headline>Jewelery Repossession in Middle Earth</headline>
+                <site-standard-profile-request>
+                    <url>https://www.linkedin.com/profile/view?id=</url>
+                </site-standard-profile-request>
+            </person>
+    """
+
+tree = ET.parse('test.xml')
+root = tree.getroot()
+print root
+
+
+
+# bsXML = BeautifulSoup(xml)
+
+# print bsXML.person.findAll("id")
+
+# def getText(nodelist):
+    # rc = []
+    # for node in nodelist:
+        # if node.nodeType == node.TEXT_NODE:
+            # rc.append(node.data)
+    # return ''.join(rc)
+
+# xmldoc = minidom.parse('test.xml')
+# print getText(xmldoc)
+# print getText(xmldoc.childNodes)
+# print xmldoc.childNodes.childNode
 # itemlist = xmldoc.getElementsByTagName('id') 
 # print len(itemlist)
 # print itemlist[0]
@@ -12,3 +43,4 @@ print xmldoc.childNodes.childNodes
 # print itemlist[0].attributes['name'].value
 # for s in itemlist :
         # print s.attributes['name'].value
+
