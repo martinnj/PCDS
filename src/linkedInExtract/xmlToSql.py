a068e3704edb4fda2dcf5bc86779ee7c42c6a144
dread-muffin <dreadmuffin@gmail.com>
2015-03-04 14:03:25 +0100

    del2 retrospective

---
+++ src/linkedInExtract/xmlToSql.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import sys
+
+from django.contrib import admin
+from models import Student
+
+class StudentAdmin(s):
+    new_entry = Student(user = s.id, first_name = s.first-name,last_name =
+            s.last-name, 
+
+
+
+
+
+
+
+
+
+StudentAdmin(sys.argv[1])
+
