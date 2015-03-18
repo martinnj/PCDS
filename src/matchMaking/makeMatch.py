27c1c72afe94e28c95363a66d4e1b0aece33d413
Henrik <henrik.bendt@gmail.com>
2015-03-18 12:55:36 +0100

    added method to compare skills of all projects and a user, but it is missing a method to get the data (skills of user and dict of project->[skills]). Thus it does not do anything yet

--- src/matchMaking/makeMatch.py
+++ src/matchMaking/makeMatch.py
@@ -1,28 +1,33 @@
 
+import operator
 
+############### HELPER METHODS ###############
 
-#Returns the degree of match between the given array of source and target skills.
-#Returns -1 if skills empty.
-#Takes a compare function compareFun, which takes a single skill and a target 
-#list of skills. It must return 1 if skill match target skills, otherwise 0.
-#Expects sourceSkills and targetSkills to be lists of strings.
-def matchSkills(sourceSkills, targetSkills, compareFun):
+#Returns the degree of match between the given list of source and target 
+#strings.
+#Returns -1 if any list is empty.
+#Takes a compare function compareFun, which takes a single string s and a target 
+#list l of strings s_i. It must return 1 if s matches any target string s_i, 
+#otherwise 0.
+def matchListsOfStrings(sourceList, targetList, compareFun):
     #Clear for empty strings and test for empty lists.
     emptyString = ""
-    while emptyString in sourceSkills:
-        sourceSkills.remove(emptyString) 
-    while emptyString in targetSkills:
-        targetSkills.remove(emptyString) 
+    while emptyString in sourceList:
+        sourceList.remove(emptyString) 
+    while emptyString in targetList:
+        targetList.remove(emptyString)
 
-    if not sourceSkills or not targetSkills:
+    if not sourceList or not targetList:
         return -1
 
     #Match skills.
     matchSum = 0
-    for skill in sourceSkills:
-        matchSum += compareFun(skill, targetSkills)
+    for skill in sourceList:
+        matchSum += compareFun(skill, targetList)
 
-    return matchSum / len(sourceSkills)
+    return matchSum / len(sourceList)
+
+
 
 
 #Returns 1 if string is equal to a string in stringList, otherwise 0.
@@ -34,3 +39,28 @@ def strictCompare(string, stringList):
 #except when converted to string.
 def containsStringCompare(string, stringList):
     return any(string.lower() in s.lower()  for s in stringList)
+
+
+
+
+#Returns list project names ordered by matching degree.
+#Only take skills into account, not languages, etc.
+def compareSkills():
+    output = {} #projectName -> degree of match
+    sourceSkillList = [""]
+    projectSkillDict = {} #projectName -> [skillsRequired]
+    stringComparefunction = strictCompare
+
+    #TODO Get list of source(user) skills
+    #TODO Get list of lists og target(project) skills
+
+    for projectName in projectSkillDict:
+        d = matchListsOfStrings(sourceSkillList, projectSkillDict[projectName], 
+                            stringComparefunction)
+        output[projectName] = d
+
+    #sortedProjectList = sorted(output.items(), key=operator.itemgetter(1))
+    sortedProjectList = sorted(output, key=output.get)
+
+    return sortedProjectList
+    
